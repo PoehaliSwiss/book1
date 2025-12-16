89e5185daf99dd72740d2dfa7aebc6ada79d6ac0
PoehaliSwiss <PoehaliSwiss@users.noreply.github.com>
2025-12-16 14:57:21 +0000

    deploy: 146e5da165415f05c0738da694070bb58556d639

--- assets/react-Duminjj9.js
+++ assets/react-Duminjj9.js
@@ -1,4 +1,4 @@
-import{R as O}from"./index-BqvcycIA.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-7lchh9yT.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
+import{R as O}from"./index-FRq7J_4h.js";import{M as P,C as A}from"./mixin-D3UDm--6.js";import{H as h}from"./hls-7lchh9yT.js";const x=t=>class extends t{static shadowRootOptions={...t.shadowRootOptions};static getTemplateHTML=(e,s={})=>{const{src:f,...T}=e;return`
         <script type="application/json" id="config">
           ${JSON.stringify(s.config||{})}
         <\/script>
