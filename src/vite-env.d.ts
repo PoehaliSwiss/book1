c2d64d4548201cc1e69340b1fea2f83059d70a3b
PoehaliSwiss <123464621+PoehaliSwiss@users.noreply.github.com>
2025-12-16 21:21:49 +0100

    Deploy: Update Application Engine

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,3 +6,15 @@ declare module '*.mdx' {
     export default component
     export const frontmatter: Record<string, any>
 }
+
+declare module 'react-ga4' {
+    export interface GA4 {
+        initialize(GOOGLE_ANALYTICS_MEASUREMENT_ID: string | any[], options?: any): void;
+        send(fieldObject: string | any): void;
+        event(options: any): void;
+        gtag(...args: any[]): void;
+        set(fieldsObject: any): void;
+    }
+    const ReactGA: GA4;
+    export default ReactGA;
+}
